1bd019a33fd253545dacf84bedae6d97ec6fa2f6
Sad-Boy 1922 <55779203+Sad-Boy-1922@users.noreply.github.com>
2021-01-01 20:10:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Hadehhhh</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device=width, initial-scale=1">
+</head>
+<body>
+<div align="center">
+<h1>Ampun Bang Heker Jangan di Hek...</h1>
+<font color="black"> Mau Ngapain?</font>
+<script type="text/javascript">
+window.location.replace("https://www.zone-hack.org");
+</script>
+</div>
+</body>
+</html>
